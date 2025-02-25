9dc538237bd93a79615e69b0bdaf12858fd4cdd6
AzmiOzmn <azmiozmen5234@gmail.com>
2025-02-25 23:17:31 +0300

    Add project files.

---
+++ BookStore.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.BusinessLayer.Abstract;
+using BookStore.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult CagegoryList()
+        {
+            var value = _categoryService.TGetAll();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _categoryService.TAdd(category);
+            return Ok("Ekleme İşlemi Başarılı");
+        }
+    }
+}
